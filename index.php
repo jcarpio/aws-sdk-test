07fd7f8afc75a824b93ac0eec906af3b9710f563
root <root@ip-172-31-12-196.eu-central-1.compute.internal>
2017-12-20 19:45:15 +0000

    terminado

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+$region = "eu-central-1";
+$key = "AKIAJU4DSS6KS4I6L3JQ";
+$secret = "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";
+putenv('AWS_DEFAULT_REGION=' . $region);
+putenv('AWS_ACCESS_KEY_ID=' . $key);
+putenv('AWS_SECRET_ACCESS_KEY=' . $secret);
+
+$GLOBALS['salida'] = shell_exec("/usr/bin/aws ec2 describe-instances --instance-ids i-0d1b835cb2cd70ea2 --output text | grep -w STATE | awk '{print $3}'");
+$GLOBALS['subject'] =  htmlspecialchars($_POST['subject']);
+
+  if ($GLOBALS['subject'] == 'start') {
+     shell_exec("/usr/bin/aws ec2 start-instances --instance-ids i-07841f41780b1cbff --output text | grep -w CURRENTSTATE | awk '{print $3}'");
+      //action for update here
+  } else if ($GLOBALS['subject'] == 'stop') {
+       shell_exec("/usr/bin/aws ec2 stop-instances --instance-ids i-07841f41780b1cbff --output text | grep -w CURRENTSTATE | awk '{print $3}'");    
+  } else {
+    // None
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,6 +42,30 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    <h1><?php echo $GLOBALS['salida']; ?></h1>
+<form action="index.php" method="post">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Instance ID</th>
+      <th scope="col">Status</th>
+      <th scope="col">Start</th>
+      <th scope="col">Stop</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>i-07841f41780b1cbff</td>
+      <td><span class="badge badge-warning"><?php echo $GLOBALS['salida']; ?></spam></td>
+      <td><button type="submit" class="btn btn-primary" name="subject" value="start">Start</button></td>
+      <td><button type="submit" class="btn btn-primary" name="subject" value="stop">Stop</button></td>
+    </tr>
+
+  </tbody>
+</table>
+</form>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
